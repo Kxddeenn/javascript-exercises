9b6a37fa678a56e75bb47f4109f29626207e6dfc
Kxddeenn <kaden.neild@gmail.com>
2024-06-03 18:17:50 -0400

    Exercise 5 - Sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,49 @@
-const sumAll = function() {
+const sumAll = function(numberstart, numberTo) {
+
+    // Declare variables
+   let sumEverything = 0;
+   let startNum = 0;
+   let endNum = 0;
+
+    
+
+   // Negative numbers return ERROR
+
+   if(numberstart < 0 || numberTo < 0){
+    return "ERROR";
+   }
+
+   if(!Number.isInteger(numberstart) || !Number.isInteger(numberTo)){
+    return "ERROR";
+   }
+
+
+   
+   // Making sure bigger number always ends the loop
+   
+   if (numberTo < numberstart){
+        endNum = numberstart;
+        startNum = numberTo;
+    }
+
+    else{
+        endNum = numberTo;
+        startNum = numberstart;
+    }
+
+
+
+    // Summing up the numbers
+
+    for(let i = startNum; i <= endNum; i++){
+
+        sumEverything += i;
+
+    } 
+
+    return sumEverything;
+
+
 
 };
 
