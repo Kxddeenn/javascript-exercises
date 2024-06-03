da97360f8213d659da87559405dc481cae384e0e
Kxddeenn <kaden.neild@gmail.com>
2024-06-03 17:53:29 -0400

    Exercise 4 - Reemoving from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...takeaway) {
+
+   let newArray = [];
+   
+    array.forEach((item) => {
+
+        if (!takeaway.includes(item)){
+            newArray.push(item);
+        }
+
+    });
+
+    return newArray;
+
 };
 
 // Do not edit below this line
