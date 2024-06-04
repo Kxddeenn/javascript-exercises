9d20e9061ed296f11749c9c408005eb257cb6209
Kxddeenn <kaden.neild@gmail.com>
2024-06-04 11:55:39 -0400

    Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+   return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	return firstNumber - secondNumber;
 };
 
-const sum = function() {
+const sum = function(array) {
 	
+  let sumArray = 0;
+  for (let i = 0 ; i < array.length; i++){
+    sumArray += array[i];
+  }
+
+  return sumArray;
+
 };
 
-const multiply = function() {
+const multiply = function(array) {
+
+  let productArray = 1;
+  for (let i = 0 ; i < array.length; i++){
+    productArray *= array[i];
+  }
+
+  return productArray;
 
 };
 
-const power = function() {
+const power = function(number, power) {
 	
+  return Math.pow(number,power);
+
 };
 
-const factorial = function() {
+const factorial = function(number) {
 	
+  let factorialResult = 1;
+
+  if ( number === 0 ){
+    return factorialResult;
+  }
+
+  for ( let i = 1 ; i <= number ; i++){
+
+    factorialResult *= i;
+
+  }
+
+  return factorialResult;
+
 };
 
 // Do not edit below this line
