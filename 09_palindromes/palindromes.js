6d341b12900524e126507a80c848152a2675ee70
Kxddeenn <kaden.neild@gmail.com>
2024-06-04 12:13:20 -0400

    Palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    initialString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    
+    reverseString = "";
+
+    for( i = initialString.length-1 ; i >= 0 ; i--){
+        reverseString += initialString[i];
+    }
+
+    if (initialString === reverseString){
+        return true;
+    }
+
+    else{
+        return false;
+    }
+
 
 };
 
