67a2fa0051cd5c6e8218b1f3d638965cecec37c4
Kxddeenn <kaden.neild@gmail.com>
2024-06-04 15:40:07 -0400

    Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+    number = parseInt(number, 10);
+   
+    if (number < 0){
+        return "OOPS";
+    }
+
+    else if (number === 0){
+        return 0;
+    }
+    
+    
+    let fibonacciLine = [0,1];
+    
+
+    for( let i = 2 ; i <= number ; i++){
+        fibonacciLine[i] = fibonacciLine[i-1] + fibonacciLine[i-2];
+    }
+
+    return fibonacciLine[number]
 
 };
 
